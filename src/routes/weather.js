a48ed07ad371ed159cf166135d02cfa2411ba2ad
Pol ENAULT <pol.enault@kleegroup.com>
2018-09-17 16:00:00 +0200

    D:/Programmes/Git/weather now returns forecast for all airports | IATA_map added

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -3,6 +3,8 @@ import {
 } from 'express';
 import axios from 'axios';
 import config from '../../config.json';
+import usable_data from '../ressources/usable_data.json';
+import IATA_map from '../ressources/IATA_map.json';
 import errorHandler from '../middleware/errorHandler';
 
 
@@ -13,14 +15,25 @@ export default () => {
 		'x-apiKey': config.aero_apiKey
 	};
 
-	// http://localhost:3000/api/weather/SFO
-	weather.get('/:airport', errorHandler(async (req, res, next) => {
-		const result = await axios({
-			method: 'get',
-			url: config.aero_rootUrl + req.params.airport, 
-			headers: axiosConfig
-		});
-		res.send(result.data);
+	// http://localhost:3000/api/weather/
+	weather.get('/', errorHandler(async (req, res, next) => {
+
+		// returns the list of IATA codes of airports
+		let tab = usable_data.weatherAirports;
+		let out = [];
+		for (let index = 0; index < tab.length; index++) {
+			let r = await axios({
+				method: 'get',
+				url: config.aero_rootUrl + tab[index], 
+				headers: axiosConfig
+			});
+			
+			// from the IATA code, adds the city name to the returned object
+			let city = IATA_map.filter(x => x.IATA === tab[index])[0].cityName;
+			r.data.currentWeather.city = city;
+			out.push(r.data.currentWeather);			
+		}
+		res.send(out);
 	}));
 
 	return weather;
